170240031457b5c98727a3567530e97ddd5e376b
Ivan Malison <IvanMalison@gmail.com>
2019-06-03 01:38:13 -0700

    Use binary-orphans_1_0_1 in overlay

--- overlay.nix
+++ overlay.nix
@@ -6,15 +6,10 @@ pkgs: pkgs.haskellPackages.override (old: {
     });
     binary-instances = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.binary-instances (old: {
       libraryHaskellDepends = old.libraryHaskellDepends ++ [
-        self.binary-orphans
+        self.binary-orphans_1_0_1
       ];
     }));
-    binary-orphans = pkgs.haskell.lib.overrideCabal super.binary-orphans (_: {
-      version = "1.0.1";
-      sha256 = "0gbmn5rpvyxhw5bxjmxwld6918lslv03b2f6hshssaw1il5x86j3";
-      revision = null;
-      editedCabalFile = null;
-    });
+    binary-orphans_1_0_1 = pkgs.haskell.lib.dontCheck super.binary-orphans_1_0_1;
     github = pkgs.haskell.lib.overrideCabal super.github (old: {
       version = "0.22";
       sha256 = "15py79qcpj0k331i42njgwkirwyiacbc5razmxnm4672dvvip2qk";
