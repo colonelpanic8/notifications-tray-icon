f28288849a39feec8972a4181ce18ccdde6cc483
Ivan Malison <IvanMalison@gmail.com>
2019-06-04 21:43:07 -0700

    Use more standard overlay file

--- default.nix
+++ default.nix
@@ -1,6 +1,3 @@
 let
-  overlay = (_: pkgs: {
-    haskellPackages = (import ./overlay.nix) pkgs;
-  });
-  pkgs = import <nixpkgs> { overlays = [ overlay ]; };
-in pkgs.haskellPackages.callPackage ./notifications-tray-icon.nix { }
+  pkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; };
+in pkgs.haskellPackages.callCabal2nix "notifications-tray-icon" ./. { }
