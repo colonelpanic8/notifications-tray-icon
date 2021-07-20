43085ad105a1e0edaa5f0e88debb5832c4507bf7
Ivan Malison <IvanMalison@gmail.com>
2021-07-19 19:21:30 -0600

    Add a nix-flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Notifications Tray Icon";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    git-ignore-nix.url = "github:IvanMalison/gitignore.nix/master";
+  };
+  outputs = { self, flake-utils, nixpkgs, git-ignore-nix }:
+  flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlay = final: prev: {
+          haskellPackages = prev.haskellPackages.override (old: {
+            overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
+            (hself: hsuper: {
+              notifications-tray-icon =
+                hself.callCabal2nix "notifications-tray-icon"
+                (git-ignore-nix.gitIgnoreSource ./.)
+                { };
+            });
+          });
+        };
+        overlays = [overlay];
+        pkgs = import nixpkgs { inherit system overlays; config.allowBroken = true; };
+      in
+      rec {
+        inherit overlay;
+        devShell = pkgs.haskellPackages.shellFor {
+          packages = p: [ p.notifications-tray-icon ];
+        };
+        defaultPackage = pkgs.haskellPackages.notifications-tray-icon;
+      });
+}
