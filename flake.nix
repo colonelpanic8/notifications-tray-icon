0d83e7fba0592bec4566662140ab197491d99126
Ivan Malison <IvanMalison@gmail.com>
2021-07-19 19:27:47 -0600

    Fix overlay in flake

--- flake.nix
+++ flake.nix
@@ -5,27 +5,26 @@
     git-ignore-nix.url = "github:IvanMalison/gitignore.nix/master";
   };
   outputs = { self, flake-utils, nixpkgs, git-ignore-nix }:
-  flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlay = final: prev: {
-          haskellPackages = prev.haskellPackages.override (old: {
-            overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
-            (hself: hsuper: {
-              notifications-tray-icon =
-                hself.callCabal2nix "notifications-tray-icon"
-                (git-ignore-nix.gitIgnoreSource ./.)
-                { };
-            });
-          });
-        };
-        overlays = [overlay];
-        pkgs = import nixpkgs { inherit system overlays; config.allowBroken = true; };
-      in
-      rec {
-        inherit overlay;
-        devShell = pkgs.haskellPackages.shellFor {
-          packages = p: [ p.notifications-tray-icon ];
-        };
-        defaultPackage = pkgs.haskellPackages.notifications-tray-icon;
+  let
+    overlay = final: prev: {
+      haskellPackages = prev.haskellPackages.override (old: {
+        overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
+        (hself: hsuper: {
+          notifications-tray-icon =
+            hself.callCabal2nix "notifications-tray-icon"
+            (git-ignore-nix.gitIgnoreSource ./.)
+            { };
+        });
       });
+    };
+    overlays = [overlay];
+  in flake-utils.lib.eachDefaultSystem (system:
+  let pkgs = import nixpkgs { inherit system overlays; config.allowBroken = true; };
+  in
+  rec {
+    devShell = pkgs.haskellPackages.shellFor {
+      packages = p: [ p.notifications-tray-icon ];
+    };
+    defaultPackage = pkgs.haskellPackages.notifications-tray-icon;
+  }) // { inherit overlay overlays; } ;
 }
