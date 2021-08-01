6b491b6a35626cbb050a551d986d52e818213304
Ivan Malison <IvanMalison@gmail.com>
2021-07-31 21:35:01 -0600

    Remove environment

--- environment.nix
+++
@@ -1,9 +0,0 @@
-self: super:
-let
-  ourOverlay = _: pkgs: {
-    haskellPackages = pkgs.haskellPackages.override (old: {
-      overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
-      });
-    });
-  };
-in ourOverlay self super
