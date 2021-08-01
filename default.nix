1e140dbfcbadc2f0cbd0e4b8da774df411428643
Ivan Malison <IvanMalison@gmail.com>
2021-07-31 21:34:11 -0600

    Delete old nix files

--- default.nix
+++
@@ -1,3 +0,0 @@
-let
-  pkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; };
-in pkgs.haskellPackages.notifications-tray-icon
--- shell.nix
+++
@@ -1,4 +0,0 @@
-let
-  overlays = [ (import ./overlay.nix) ];
-  pkgs = (import <nixpkgs>) { inherit overlays; };
-in pkgs.haskellPackages.shellFor { packages = p: [ p.notifications-tray-icon ]; }
